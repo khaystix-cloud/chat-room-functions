2868cafe9ac9111627bcb27eb69361d9de0a816d
Solomon Adekuoroye <67961201+khaystix-cloud@users.noreply.github.com>
2020-12-14 19:45:06 +0100

    Add files via upload

---
+++ util/config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  apiKey: "AIzaSyC67_lIsaigz_rkXT-ToFe_qk6M56NIZRw",
+  authDomain: "chat-room-ad115.firebaseapp.com",
+  databaseURL: "https://chat-room-ad115.firebaseio.com",
+  projectId: "chat-room-ad115",
+  storageBucket: "chat-room-ad115.appspot.com",
+  messagingSenderId: "254224152951",
+  appId: "1:254224152951:web:67aeee7d883df7c5478972",
+  measurementId: "G-H194KK9CHN"
+};
